67726ee3320891b5dbeae578f37f608ad2d0d9db
Hugo <hugo.pievic@gmail.com>
2015-09-22 11:50:02 +0200

    form directive + empty input class

--- app/scripts/directives/form.directive.js
+++ app/scripts/directives/form.directive.js
@@ -1,35 +1,38 @@
 (function (global, angular) {
     'use strict';
 
-    angular.module('campus.app').directive('form', function () {
-        return {
-            restrict: 'A',
-            link: function ($scope, element) {
+    angular.module('campus.app').directive('form',
+        function (
+            $window
+        ) {
+            return {
+                restrict: 'A',
+                link: function ($scope, element) {
 
-                var name = element.attr('name'),
-                    $form = $scope[name];
+                    var name = element.attr('name'),
+                        $form = $scope[name];
 
-                $scope.state = {
-                    valid: false,
-                    openDatepickers: {}
-                };
+                    $scope.state = {
+                        valid: false,
+                        openDatepickers: {}
+                    };
 
-                $scope.model = {};
+                    $scope.model = {};
 
-                $scope.openDatepicker = function(name) {
-                    $scope.state.openDatepickers[name] = true;
-                };
+                    $scope.openDatepicker = function(datepickerName) {
+                        $scope.state.openDatepickers[datepickerName] = true;
+                    };
 
-                $scope.submit = function() {
-                    console.log($form);
-                    if ($form.$invalid) {
-                        $window.alert('erreur dans le formulaire ' + name);
-                    } else {
-                        console.log('TO DO: SUBMIT');
-                    }
+                    $scope.submit = function() {
+                        if ($form.$invalid) {
+                            $window.alert('erreur dans le formulaire ' + name);
+                        } else {
+                            console.log('TO DO: SUBMIT'); // eslint-disable-line
+                        }
+                    };
                 }
-            }
-        };
-    });
+            };
+        }
+    );
 
 }(window, window.angular));
