e0c89caeaa748d4556878d232f8dec9c35abbe78
Hugo <hugo.pievic@gmail.com>
2015-09-22 09:15:50 +0200

    textarea autoresize directive

--- app/scripts/directives/textareaAutosize.directive.js
+++ app/scripts/directives/textareaAutosize.directive.js
@@ -5,7 +5,16 @@
         return {
             restrict: 'A',
             link: function ($scope, element) {
-                console.log(element);       
+                console.log('textareaAutosize', element);
+                var el = element[0];
+
+                element.on('keyup input', resize);
+
+                function resize() {
+                    element
+                        .css('height', 'auto')
+                        .css('height', el.scrollHeight);
+                }
             }
         };
     });
